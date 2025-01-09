48405a272cc31f01f9e617e1ca4ffffc1a8deaff
root <root@LAPTOP-MS3I01UM>
2025-01-09 22:15:46 +0100

    codecrafters submit [skip ci]

---
+++ src/Parser.cs
@@ -0,0 +1,36 @@
+﻿namespace Parser;
+
+using Token;
+
+public class Parser(List<Token> _tokens)
+{
+    private int _current;
+
+    private bool IsAtEnd()
+    {
+        return Peek().Type == TokenType.EOF;
+    }
+
+    private Token Peek()
+    {
+        return _tokens[_current];
+    }
+
+    private Token Advance()
+    {
+        if (!IsAtEnd())
+        {
+            _current++;
+        }
+
+        return _tokens[_current - 1];
+    }
+
+    public void Parse()
+    {
+        while (!IsAtEnd())
+        {
+            Console.WriteLine(Advance().Lexeme);
+        }
+    }
+}
--- src/main.cs
+++ src/main.cs
@@ -10,26 +10,41 @@ if (args.Length < 2)
 var command = args[0];
 var filename = args[1];
 
-if (command != "tokenize")
-{
-    Console.Error.WriteLine($"Unknown command: {command}");
-    Environment.Exit(1);
-}
 
 var fileContents = File.ReadAllText(filename);
 
 //Uncomment this block to pass the first stage
 if (!string.IsNullOrEmpty(fileContents))
 {
-    Tokenizer tokenizer = new(fileContents);
-    tokenizer.Scan();
-    
-    foreach (var token in tokenizer.GetTokens())
+    switch (command)
     {
-        Console.WriteLine(token);
+        case "tokenize":
+        {
+            Tokenizer tokenizer = new(fileContents);
+            tokenizer.Scan();
+        
+            foreach (var token in tokenizer.GetTokens())
+            {
+                Console.WriteLine(token);
+            }
+        
+            Environment.Exit(tokenizer.GetReturnCode());
+            break;
+        }
+        case "parse":
+        {
+            Tokenizer tokenizer = new(fileContents);
+            tokenizer.Scan();
+            
+            Parser.Parser parser = new(tokenizer.GetTokens());
+            parser.Parse();
+            break;
+        }
+        default:
+            Console.Error.WriteLine($"Unknown command: {command}");
+            Environment.Exit(1);
+            break;
     }
-    
-    Environment.Exit(tokenizer.GetReturnCode());
 } else {
     Console.WriteLine("EOF  null");
     Environment.Exit(0);
