08e1a8398a6c36ec2af5b3a78b696f15e8009ded
root <root@LAPTOP-MS3I01UM>
2025-01-11 19:30:02 +0100

    codecrafters submit [skip ci]

--- src/ast/Interpreter.cs
+++ src/ast/Interpreter.cs
@@ -41,7 +41,7 @@ public class ExpressionInterpreter : IVisitor<object?>
     {
         var right = _expression.Right.Accept(this);
 
-        switch(_expression.Operator.Type)
+        switch (_expression.Operator.Type)
         {
             case Token.TokenType.MINUS:
                 CheckNumberOperand(_expression.Operator, right);
@@ -58,25 +58,43 @@ public class ExpressionInterpreter : IVisitor<object?>
         var left  = _expression.Left.Accept(this);
         var right = _expression.Right.Accept(this);
 
-        return _expression.Operator.Type switch
+        switch (_expression.Operator.Type)
         {
-            Token.TokenType.MINUS => (double)left - (double)right,
-            Token.TokenType.SLASH => (double)left / (double)right,
-            Token.TokenType.STAR  => (double)left * (double)right,
-            Token.TokenType.PLUS => left switch
-            {
-                double number => number + (double)right,
-                string text   => text   + right,
-                _             => null
-            },
-            Token.TokenType.GREATER       => (double)left > (double)right,
-            Token.TokenType.GREATER_EQUAL => (double)left >= (double)right,
-            Token.TokenType.LESS          => (double)left < (double)right,
-            Token.TokenType.LESS_EQUAL    => (double)left <= (double)right,
-            Token.TokenType.BANG_EQUAL    => !IsEqual(left, right),
-            Token.TokenType.EQUAL_EQUAL   => IsEqual(left, right),
-            _                             => null
-        };
+            case Token.TokenType.MINUS:
+                CheckNumberOperands(_expression.Operator, left, right);
+                return (double)left - (double)right;
+            case Token.TokenType.SLASH:
+                CheckNumberOperands(_expression.Operator, left, right);
+                return (double)left / (double)right;
+            case Token.TokenType.STAR:
+                CheckNumberOperands(_expression.Operator, left, right);
+                return (double)left * (double)right;
+            case Token.TokenType.PLUS:
+                return left switch
+                {
+                    double dLeft when right is double dRight => dLeft + dRight,
+                    string sLeft when right is string sRight => sLeft + sRight,
+                    _ => throw new RuntimeError(_expression.Operator, "Operands must be two numbers or two strings.")
+                };
+            case Token.TokenType.GREATER:
+                CheckNumberOperands(_expression.Operator, left, right);
+                return (double)left > (double)right;
+            case Token.TokenType.GREATER_EQUAL:
+                CheckNumberOperands(_expression.Operator, left, right);
+                return (double)left >= (double)right;
+            case Token.TokenType.LESS:
+                CheckNumberOperands(_expression.Operator, left, right);
+                return (double)left < (double)right;
+            case Token.TokenType.LESS_EQUAL:
+                CheckNumberOperands(_expression.Operator, left, right);
+                return (double)left <= (double)right;
+            case Token.TokenType.BANG_EQUAL:
+                return !IsEqual(left, right);
+            case Token.TokenType.EQUAL_EQUAL:
+                return IsEqual(left, right);
+            default:
+                return null;
+        }
     }
 
     private static bool IsTruthy(object? _value)
@@ -98,10 +116,16 @@ public class ExpressionInterpreter : IVisitor<object?>
             _                        => _left.Equals(_right)
         };
     }
-    
-    private void CheckNumberOperand(Token.Token _operator, object? _operand)
+
+    private static void CheckNumberOperand(Token.Token _operator, object? _operand)
     {
         if (_operand is double) return;
         throw new RuntimeError(_operator, "Operand must be a number.");
+    }
+
+    private static void CheckNumberOperands(Token.Token _operator, object? _left, object? _right)
+    {
+        if (_left is double && _right is double) return;
+        throw new RuntimeError(_operator, "Operands must be numbers.");
     }
 }
